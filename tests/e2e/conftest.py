47f3986eb630ddcbf5e24a08b4170f81b5690879
Karyna Konopelko <karyna_konopelko@epam.com>
2025-10-03 16:56:28 +0300

    update: add browser manager, move browser options initialization to the separate class

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -1,21 +1,18 @@
 import pytest
 
 from playwright.sync_api import sync_playwright
-from config.config_manager import config
+from browser.browser_manager import *
 
 @pytest.fixture(scope="session", autouse=True)
 def browser():
     with sync_playwright() as pw:
-        browser = pw.chromium.launch(headless=config.headless_mode)
+        browser = get_browser_type(pw).launch(**get_launch_options())
         yield browser
         browser.close()
 
 @pytest.fixture(scope="function")
 def page(browser):
-    context = browser.new_context()
-    page = context.new_page(ignore_https_errors=True)
+    context = browser.new_context(**get_context_options())
+    page = context.new_page()
     yield page
     context.close()
-
-
-
---
+++ web/browser/browser_manager.py
@@ -0,0 +1,35 @@
+from playwright.sync_api import Playwright, BrowserType
+from config.config_manager import config
+
+def get_browser_type(playwright: Playwright) -> BrowserType:
+        browser = config.browser_type
+
+        if browser == 'chromium':
+            return playwright.chromium
+
+        elif browser == 'firefox':
+            return playwright.firefox
+
+        elif browser == 'webkit':
+            return playwright.webkit
+        else:
+            raise ValueError(f"Unsupported browser: {browser}")
+        
+def get_launch_options():
+        launch_options = {
+            "headless": config.headless_mode,
+            "args": ["--start-maximized"] # for non-headless mode, doesn't work
+        }
+        return launch_options
+
+def get_context_options():        
+        if config.headless_mode:
+            context_options = {
+                "viewport": {"width": 1920, "height": 1080} # for headless mode
+            }
+            return context_options
+        else:
+            context_options = {
+                "viewport": None  # important! disable fixed viewport
+            }
+        return context_options
