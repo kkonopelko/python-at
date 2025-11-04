d2e6aec40d3d0157349c4574e4001db82b2147d4
Karyna Konopelko <karyna_konopelko@epam.com>
2025-11-04 18:48:34 +0200

    update: ci file, config file

--- tests/playground/test_calculator.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from playground.calculator import Calculator
-
-
-class TestCalculator:
-    
-    @pytest.mark.parametrize(" x, y, result", [(1, 2, 0.5), (5, -1, -5)])
-    def test_divide(self, x, y, result):
-        assert Calculator().divide(x, y) == result
--- tests/playground/test_main.py
+++
@@ -1,9 +0,0 @@
-from playground.main import A
-
-def test_main():
-    assert A.x == 1
-
-def test_sum():
-    x = 1
-    y = 2
-    assert x + y == 3
